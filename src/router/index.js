203d24411c83ad5e63503b269cdc79aaf5d8963a
PyDa5 <1174446068@qq.com>
2022-05-05 13:06:53 +0800

    优化配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,41 @@
+/**
+ * 模块： 路由配置
+ * 时间： 2022-5-5 13:01:37
+ * 说明：
+ */
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { path_route } from '@/utils/route'
 
-// ------------------------------- 配置嵌套路由或者命名不规则路由 -------------------------------
+
+/**
+ * 功能： 配置嵌套路由或者命名不规则的路由
+ * 示例：
+ *  {
+ *    path: '/',
+ *    name: 'index',
+ *    componet: () => import('../views/Index.vue')
+ *  },
+ */
 let routes = [
-  // {
-  //   path: '/',
-  //   name: 'index',
-  //   componet: () => import('../views/Index.vue')
-  // },
+
 ]
 
-/** 配置简单路由，根据URL匹配组件，组件名称首字母大写
+
+/**功能：配置简单路由
+ * 根据URL匹配组件，组件名称首字母大写
  * :path URL路径
  * :name URL名称
  * :component: () => import('../views/${path}.vue)
  */
-const INSTALLED_PAGES = {
+ const INSTALLED_PAGES = {
   'login': '登陆',
   'index': '首页',
   'demo': 'Demo',
   'order/import': '订单导入'
 }
 
-// 配置index路由
+
+// 配置index根路由
 routes.push({
   path: '/',
   redirect: '/index'
@@ -30,14 +43,12 @@ routes.push({
 
 
 // ******************************************* 配置到此结束 *********************************
-
-
+// path_route生成路由
 for(var path in INSTALLED_PAGES){
   var component_name = INSTALLED_PAGES[path]
   path = path.startsWith('/') ? path : '/'+path
   routes.push(path_route(path, component_name))
 }
-
 
 // 创建router
 const router = createRouter({
